4aafb3a3fea9aa9a540b1ec3c17bafc6422841df
ABDULLAHI <Ashleyalvins010@gmail.com>
2024-01-26 16:30:30 +0100

    added tsconfig.json

---
+++ scr/post/types.ts
@@ -0,0 +1,13 @@
+export declare type Post = {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+};
+
+export declare type createPosts = {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+};
